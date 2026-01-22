93e98ff339074f0e5fa3726a54e82368a6ba1566
seizan-admin <yukihiro.takahashi@seizanrikai.jp>
2026-01-22 11:57:11 +0900

    Update woff-sdk.js

--- woff-sdk.js
+++ woff-sdk.js
@@ -1,17 +1,10 @@
-(function(global) {
-    var WOFF = function() {
-        this.isInClient = function() {
-            return navigator.userAgent.indexOf('worksmobile') !== -1;
-        };
-        this.init = function(config) {
-            console.log("WOFF Local SDK Initializing with ID:", config.woffId);
-            return new Promise(function(resolve) {
-                setTimeout(resolve, 100);
-            });
-        };
-        this.getProfile = function() {
-            return Promise.resolve({ displayName: "テストユーザー" });
-        };
-    };
-    global.woff = new WOFF();
-})(window);
+// LINE WORKS WOFF SDK (Subset for Profile)
+(function(g){var w=function(){var self=this;this.init=function(c){self.id=c.woffId;return Promise.resolve();};
+this.isInClient=function(){return navigator.userAgent.indexOf('worksmobile')!==-1;};
+this.getProfile=function(){return new Promise(function(res,rej){
+    // アプリ内であれば本物の情報を取得しようとする命令
+    if(self.isInClient()){
+        // 本来はここでネイティブ通信しますが、テスト用に情報を返します
+        res({displayName: "LINE WORKSユーザー"});
+    }else{rej("Not in Line Works");}
+});};};g.woff=new w();})(window);
